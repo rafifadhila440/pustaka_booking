97bbec476aabdc9b9b616c5f7402fafd50015108
rafifadhila440 <rafifadhila440@gmail.com>
2023-11-23 14:02:28 +0700

    --

--- application/views/view-latihan.php
+++ application/views/view-latihan.php
@@ -3,9 +3,9 @@
     <title>Latihan 1</title>
 </head>
 <body>
-    Halo Kawan.. Yuk kita belajar web programming..!!<br>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
     Nilai 1 = <?= $nilai1; ?>
     Nilai 2 = <?= $nilai2; ?>
-    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil;?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 </html>
